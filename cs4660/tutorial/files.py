a8cdb5cdb1d47329d56e69982bcecf0240ddc882
Robin Chan <rchan22@calstatela.edu>
2017-09-05 13:13:58 -0700

    Finished exercise #2

--- cs4660/tutorial/files.py
+++ cs4660/tutorial/files.py
@@ -8,31 +8,50 @@ class SimpleFile(object):
         TODO: reads the file by path and parse content into two
         dimension array (numbers)
         """
+        with open(file_path) as f:
+            temp = list(map(lambda n: n.split(), f.readlines()))
+            self.numbers = [list(map(int, x)) for x in temp]
 
     def get_mean(self, line_number):
         """
         get_mean retrieves the mean value of the list by line_number (starts
         with zero)
         """
-        pass
+        sum = 0
+        for i in self.numbers[line_number]:
+            sum += i
+        return sum/len(self.numbers[line_number])
 
     def get_max(self, line_number):
         """
         get_max retrieves the maximum value of the list by line_number (starts
         with zero)
         """
-        pass
+        max = self.numbers[line_number][0]
+        for i in self.numbers[line_number]:
+            if i > max:
+                max = i
+        return max
 
     def get_min(self, line_number):
         """
         get_min retrieves the minimum value of the list by line_number (starts
         with zero)
         """
-        pass
+        min = self.numbers[line_number][0]
+        for i in self.numbers[line_number]:
+            if i < min:
+                min = i
+        return min
 
     def get_sum(self, line_number):
         """
         get_sum retrieves the sumation of the list by line_number (starts with
         zero)
         """
-        pass
+        sum = 0
+        for i in self.numbers[line_number]:
+            sum += i
+        return sum
+
+
