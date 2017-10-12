55e90e3a088d44ae124daf2c2a9dde9d650873c3
Robin Chan <rchan22@calstatela.edu>
2017-10-11 20:20:28 -0700

    finished Dijkstra search

--- cs4660/search/searches.py
+++ cs4660/search/searches.py
@@ -15,7 +15,7 @@ def bfs(graph, initial_node, dest_node):
     q = queue.deque()
     q.append(initial_node)
 
-    while len(q) > 0:
+    while q:
         current_node = q.popleft()
 
         if current_node == dest_node:
@@ -48,7 +48,31 @@ def dijkstra_search(graph, initial_node, dest_node):
     uses graph to do search from the initial_node to dest_node
     returns a list of actions going from the initial node to dest_node
     """
-    pass
+    distances = {initial_node: 0}
+    parents = {}
+    q = queue.PriorityQueue()
+    q.put((0, initial_node))
+
+    while q:
+        heap_element = q.get()
+        current_node = heap_element[1]
+
+        if current_node == dest_node:
+            temp_node = dest_node
+            path = []
+            while temp_node in parents:
+                value = distances[temp_node]
+                path.append(g.Edge(parents[temp_node], temp_node, value))
+                temp_node = parents[temp_node]
+            path.reverse()
+            return path
+
+        for neighbor in graph.neighbors(current_node):
+            adj_distance = distances[current_node] + graph.distance(current_node, neighbor)
+            if neighbor not in distances or adj_distance < distances[neighbor]:
+                distances[neighbor] = graph.distance(current_node, neighbor)
+                parents[neighbor] = current_node
+                q.put((distances[neighbor],neighbor))
 
 def a_star_search(graph, initial_node, dest_node):
     """
